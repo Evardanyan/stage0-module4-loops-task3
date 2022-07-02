6bcea57c4babf0c32fc619e23acff5ccba4ba801
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-07-03 03:53:07 +0400

    first_try

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        String tempLength = "" + t;
+        int result =  0;
+        for (int i = 0; i <= tempLength.length() ; i++) {
+            result = result + t % 10;
+            System.out.println("<<<<<" + result);
+            t = t / 10;
+            System.out.println(">>>>>>" + t);
+        }
+        System.out.println(result);
 
     }
 }
